574a636c1e1b935df6787f66fe5b1e039bf1503e
Eduardo AlvareZ <udealvarez@icloud.com>
2025-07-16 21:59:14 -0300

    GridView y AutoGenerateColumns

---
+++ Unidad_4_Eventos/4-GridView-AutoGenerateColumns/4-GridView-AutoGenerateColumns/Default.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Dominio;
+
+namespace _4_GridView_AutoGenerateColumns
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            AutoNegocio negocio = new AutoNegocio();
+
+            dgvAutos.DataSource = negocio.listar();
+            dgvAutos.DataBind(); // DATA BIND -> ES PARA RENDERIZAR LA LISTA EN LA PANTALLA
+
+
+
+
+        }
+    }
+}
