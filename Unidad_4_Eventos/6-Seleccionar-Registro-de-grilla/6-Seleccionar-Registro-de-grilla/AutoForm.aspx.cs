256a9f6efeea7ea09cc7189c3d37d8ad74401100
Eduardo AlvareZ <udealvarez@icloud.com>
2025-07-18 00:32:17 -0300

    seleccionar un registro de la grilla

---
+++ Unidad_4_Eventos/6-Seleccionar-Registro-de-grilla/6-Seleccionar-Registro-de-grilla/AutoForm.aspx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Dominio;
+
+namespace _6_Seleccionar_Registro_de_grilla
+{
+    public partial class AutoForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                ddlColores.Items.Add("negro");
+                ddlColores.Items.Add("blanco");
+                ddlColores.Items.Add("rojo");
+            }
+        }
+
+        protected void btnAceptar_Click(object sender, EventArgs e)
+        {
+            Auto a = new Auto();
+            a.Id = int.Parse(txtId.Text);
+            a.Modelo = txtModelo.Text;
+            a.Descripcion = txtDesripcion.Text;
+            a.Color = ddlColores.SelectedValue; 
+            a.Fecha = DateTime.Parse(txtFecha.Text); 
+            a.Usado = chkUsado.Checked;
+
+            // FORMA FACIL
+            if (rdbImportado.Checked)
+            {
+                a.Importado = true;
+            }
+            else if (rdbNacional.Checked) 
+            {
+                a.Importado = false;
+            }
+
+            // DE ACA A LA BASE DE DATOS METIANTE ALGUN METODO
+
+            // forma larga de cargar en memoria un registro nuevo
+   //         ((List<Auto>)Session["listaAutos"]).Add(a);   
+
+            //forma corta      
+            List<Auto> temporal = (List<Auto>)Session["listaAutos"];
+            temporal.Add(a); // aca le agrego el auto a la lista TEMPORAL
+
+
+            Response.Redirect("Default.aspx");
+        }
+    }
+}
---
+++ Unidad_4_Eventos/6-Seleccionar-Registro-de-grilla/6-Seleccionar-Registro-de-grilla/Default.aspx.cs
@@ -0,0 +1,30 @@
+﻿using Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _6_Seleccionar_Registro_de_grilla
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["listaAutos"] == null)
+            {
+                AutoNegocio negocio = new AutoNegocio();
+                Session.Add("listaAutos", negocio.listar());
+            }
+
+            dgvAutos.DataSource = Session["listaAutos"];
+            dgvAutos.DataBind(); // DATA BIND -> ES PARA RENDERIZAR LA LISTA EN LA PANTALLA
+        }
+
+        protected void dgvAutos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var algo = dgvAutos.SelectedRow.Cells[0].Text;
+        }
+    }
+}
