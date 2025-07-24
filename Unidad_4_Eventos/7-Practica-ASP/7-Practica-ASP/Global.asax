a0a4725bd29f914198208645b48a477f62c4fd61
Eduardo AlvareZ <udealvarez@icloud.com>
2025-07-24 00:28:58 -0300

    practica ASP

---
+++ Unidad_4_Eventos/7-Practica-ASP/7-Practica-ASP/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_7_Practica_ASP.Global" Language="C#" %>
---
+++ Unidad_4_Eventos/7-Practica-ASP/Dominio/Direccion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Direccion
+    {
+        public string Calle { get; set; }
+        public int Altura { get; set; }
+    }
+}
