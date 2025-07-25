19b5bce608950d6e753e4517a976d97da3381b93
Eduardo AlvareZ <udealvarez@icloud.com>
2025-07-25 01:50:08 -0300

    update practica

---
+++ Unidad_4_Eventos/7-Practica-ASP/7-Practica-ASP/Default.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="_7_Practica_ASP.Default" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+</asp:Content>
--- Unidad_4_Eventos/7-Practica-ASP/Dominio/Direccion.cs
+++ Unidad_4_Eventos/7-Practica-ASP/Dominio/Direccion.cs
@@ -11,4 +11,24 @@ namespace Dominio
         public string Calle { get; set; }
         public int Altura { get; set; }
     }
+
+    public class DireccionNegocio
+    {
+        List<Direccion> listarDirecciones() 
+        {
+        
+            List<Direccion> listaDirec = new List<Direccion>();
+
+            listaDirec.Add(new Direccion());
+            listaDirec.Add(new Direccion());
+
+            listaDirec[0].Calle = "Pardo";
+            listaDirec[0].Altura = 1851;
+
+            listaDirec[1].Calle = "Alsina";
+            listaDirec[1].Altura = 1275;
+
+            return listaDirec;
+        }
+    }
 }
