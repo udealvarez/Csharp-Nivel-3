8ff2c2ff0c87fe346852630359b358dba4572d6f
Eduardo AlvareZ <udealvarez@icloud.com>
2025-08-09 00:49:14 -0300

    feat: invalidar poke

---
+++ Unidad_6_DesarrollandoApp/6-Inactivar-pokemon/6-Inactivar-pokemon/Default.aspx.cs
@@ -0,0 +1,33 @@
+﻿using negocio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using dominio;
+
+namespace _2_Listar_con_SP
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        public List<Pokemon> ListaPokemon { get; set; }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            PokemonNegocio negocio = new PokemonNegocio();
+            ListaPokemon = negocio.listarConSP();
+
+            if (!IsPostBack)
+            {
+                repRepetidor.DataSource = ListaPokemon;
+                repRepetidor.DataBind();
+            }
+        }
+
+        protected void btnEjemplo_Click(object sender, EventArgs e)
+        {
+            string valor = ((Button)sender).CommandArgument; /*el CommandArgument es el pase que yo le pase en el argumento*/
+        }
+    }
+}
---
+++ Unidad_6_DesarrollandoApp/6-Inactivar-pokemon/6-Inactivar-pokemon/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_6_Inactivar_pokemon.Global" Language="C#" %>
